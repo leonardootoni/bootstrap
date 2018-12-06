4868df190a85835d26aa395b7480b4375e5b17db
Leonardo Otoni <leonardootoni@gmail.com>
2018-12-06 13:14:38 -0500

    Finished See what you do section + embeded youtube modal player working

--- projects/glozzom/js/main.js
+++ projects/glozzom/js/main.js
@@ -1,6 +1,12 @@
 //get the current year for the copyright
 $('#footer-year').text(new Date().getFullYear());
 
+//configure slider <<carousel>>
+$(".carousel").carousel({
+    interval: 6000,
+    pause: "hover"
+});
+
 //enable scrollspy for #main-nav
 $('body').scrollspy({
     target: '#main-nav'
@@ -20,4 +26,15 @@ $("#main-nav a").on("click", function (event) {
             }
         );
     }
+});
+
+//It stops the youtube player after to close (hide) the Modal.
+$("#videoModal").on('hidden.bs.modal', function (e) {
+    $("#youtube-video-iframe").attr("src", $("#youtube-video-iframe").attr("src"));
+});
+
+//Start to play a youtube video from a provided id
+$("#link-video-player").on("click", function (event) {
+    const link = $("#link-video-player").attr("data-video");
+    const iframe = $("#youtube-video-iframe").attr("src", link);
 });
